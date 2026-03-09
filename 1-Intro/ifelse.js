1e78f5961409a60058bcd7ea830d2712c2437607
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-09 20:14:49 +0500

    adding if else statements
    control statements if - else and truthy and falsey value difference

---
+++ 1-Intro/ifelse.js
@@ -0,0 +1,19 @@
+/* if - else are control statements
+the contain blocks of code which execute if a condition is true or false */
+let age = 19;
+if(age>=18)
+  console.log("You are old enough to have a driving license") // execute if true
+else
+  console.log("You are not old enough to have a driving license")//execute if false
+// for executing multiple lines of code use { }
+let isStudent = true;
+let Name = "Umer"
+if(isStudent)
+  console.log(`Hi, ${Name} `) // execute if true
+else
+  console.log("You are not old enough to have a driving license")//execute if false
+console.log();
+console.log("Truthy Value:");
+console.log("true,1 ... infinity etc");
+console.log("Falsy Value:");
+console.log("false , 0 , null,undefined,Nan");
