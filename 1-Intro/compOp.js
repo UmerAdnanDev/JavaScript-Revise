656e4501ba0785d30dd93a89d5ed63da3d6ce266
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-09 06:10:30 +0500

    adding comparison operators
    here we have comparision / relational operators along with equality operators

---
+++ 1-Intro/compOp.js
@@ -0,0 +1,21 @@
+//Comparison Operators
+//Relational Operators
+console.log(2 > 3); //false
+console.log(2 < 3);//true
+console.log(2 == 3);
+console.log(12 == 12);
+console.log(4 >= 3);
+console.log(5 <= 1)
+console.log(2 != 3);
+
+// 
+console.log(10 == '10') //true just check value not type of data
+console.log("12" > "23")
+//Equality Operator : compares both data type and values
+console.log(10 === 10); // true
+console.log(12 === "12") //false
+console.log(1 !=="1") //true
+
+
+
+
